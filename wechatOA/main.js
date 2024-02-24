103cff38d0c499ca099b67a0db243f2191cef2fb
a-lhliang <a-lhliang@qq.com>
2024-02-24 16:36:21 +0800

    update

---
+++ wechatOA/main.js
@@ -0,0 +1,1 @@
+const vh=window.innerHeight;document.documentElement.style.setProperty("--vh",`${vh}px`),window.addEventListener("resize",(()=>{let e=window.innerHeight;document.documentElement.style.setProperty("--vh",`${e}px`)}));
